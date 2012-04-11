853b2d8432255b13ed691345813f4d759f38ead5
Daz J <yuki@thebronasium.com>
2012-04-11 14:34:54 +0100

    Seems to only make things worse

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/zte/blade2/device_blade2.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=blade2 BUILD_ID=ITL41D BUILD_FINGERPRINT=google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys" BUILD_NUMBER=223971
